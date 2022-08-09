81f1f3bfcf16655756f1bec13b9b538d82e79d36
Nakaishi Ryosuke <acna2676@gmail.com>
2022-08-09 16:21:22 +0900

    feat: 7

--- tdd/src/main/java/money/Money.java
+++ tdd/src/main/java/money/Money.java
@@ -1,10 +1,10 @@
 package money;
 
 public class Money {
-    protected int amount; // = 10;
+    protected int amount;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(money.getClass());
     }
 }
