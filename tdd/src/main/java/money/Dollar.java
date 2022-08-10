652f1063666756870c065b897dbc31f7dca562ef
Nakaishi Ryosuke <acna2676@gmail.com>
2022-08-10 13:41:41 +0900

    feat: 9

--- tdd/src/main/java/money/Dollar.java
+++ tdd/src/main/java/money/Dollar.java
@@ -2,13 +2,14 @@ package money;
 
 public class Dollar extends Money {
 
-    Dollar(int amount) {
-        this.amount = amount;
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
+
 
     Money times(int multiplier) {
         // this.amount *= multiplier;
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 
 }
