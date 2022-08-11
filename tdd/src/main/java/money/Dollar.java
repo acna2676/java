56ca83ffd79b9a5dc01e91456bdf51ca0be485d9
Nakaishi Ryosuke <acna2676@gmail.com>
2022-08-11 14:18:41 +0900

    feat: 10

--- tdd/src/main/java/money/Dollar.java
+++ tdd/src/main/java/money/Dollar.java
@@ -5,11 +5,4 @@ public class Dollar extends Money {
     Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-
-    Money times(int multiplier) {
-        // this.amount *= multiplier;
-        return Money.dollar(amount * multiplier);
-    }
-
 }
--- tdd/src/main/java/money/Franc.java
+++ tdd/src/main/java/money/Franc.java
@@ -5,10 +5,4 @@ public class Franc extends Money {
     Franc(int amount, String currency) {
         super(amount, currency);
     }
-
-
-    Money times(int multiplier) {
-        // this.amount *= multiplier;
-        return Money.franc(amount * multiplier);
-    }
 }
